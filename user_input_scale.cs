64069aa7c7035988fe5e70a1fc7b4cad95251806
Stephen Norfleet <30849573+stephennorfleet@users.noreply.github.com>
2018-05-25 12:46:38 -0400

    First commit

---
+++ user_input_scale.cs
@@ -0,0 +1,17 @@
+using System;
+
+class MainClass {
+
+  static string[] tones = {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F",
+     "F#","G", "G#"}; //array
+
+  public static void Main (string[] args) {
+    Console.WriteLine ("Please choose a note by typing a number 1 - 12 and then press Enter. 1 = A, 2 = A#, 3 = B, etc."); //this is the main function - starting with numbers because computers love numbers
+    string choice; //choice is the variable & prepares memory to save the user's choice
+    choice = Console.ReadLine(); //reads in the user's choice and save it in the memory
+    Console.WriteLine ("You chose " + choice + " which is the tone " + 
+    tones[Convert.ToInt32(choice)-1]); 
+    
+  }
+}
+ 
